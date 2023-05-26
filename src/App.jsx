f40bc88123d4b5a035157bd28d07b52a3538ccba
gsilveirag <gustavo.silveira.goncalves@gmail.com>
2023-05-26 17:28:58 -0300

    Entrada de um novo component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import MeuComponent from "./components/MeuComponent"
 
 function App() {
 
@@ -6,8 +7,22 @@ function App() {
     <>
       <h1>Ola mundo!</h1>
       <p>Programador de Desenvolvimento:<strong> Gustavo Gonçalves </strong></p>
+      <MeuComponent/>
+
+      <MeuBotao conteudo='me clique' />
+      <MeuBotao conteudo='depois aqui' />
+      <MeuBotao conteudo='por ultimo aqui' />
     </>
     )
+}
+
+
+
+
+function MeuBotao(props) {
+  return(
+    <button type="submit">{props.conteudo}</button>
+  )
 }
 
 export default App
---
+++ src/components/MeuComponent.jsx
@@ -0,0 +1,6 @@
+export default function MeuComponent(){
+    return(
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum fugit, eos sed saepe reprehenderit quaerat placeat aut distinctio, at, velit eum. Quibusdam suscipit, culpa quaerat quod doloremque rem consectetur est?</p>
+    )
+  }
+
